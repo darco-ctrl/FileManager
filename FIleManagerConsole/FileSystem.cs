93c4539441920e569636b3e9e4164f0ac79dc4c6
Darco <darcowb388@gmail.com>
2025-09-17 17:29:24 +0530

    print list of folders

--- FIleManagerConsole/FileSystem.cs
+++ FIleManagerConsole/FileSystem.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection.PortableExecutable;
 
 namespace FIleManagerConsole
 {
-    internal class FileSystem
+    internal static class FileSystem
     {
+        public static string current_dir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+        public static void PrintListOfFiles()
+        {
+            try
+            {
+                var entry = Directory.EnumerateFileSystemEntries(current_dir);
+
+                int counter = 0;
+                foreach (var dir in entry)
+                {
+                    counter += 1;
+
+                    Console.WriteLine($"{counter}. {dir}");
+                }
+
+            } catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine($"Path not found,\n request failed with exeption: {ex}");
+                
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"Cancel the proecces with exeption: {ex}");
+            }
+        }
     }
 }
--- FIleManagerConsole/Program.cs
+++ FIleManagerConsole/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 
-namespace Main
+namespace FIleManagerConsole
 {
     public class  Program
     {
@@ -11,8 +12,39 @@ namespace Main
 
             while (true) {
 
-                Console.WriteLine("Hell world");
+                Console.WriteLine("""
+                    What would you like to do?
+                    1. print all file in current dir
+                    """);
 
+                string? input = Console.ReadLine();
+
+                if (input == null) { continue; }
+
+                try
+                {
+                    int input_num = int.Parse(input);
+
+                    switch (input_num)
+                    {
+                        case 1:
+                            Console.WriteLine("Printing Dirs\n");
+                            FileSystem.PrintListOfFiles();
+                            break;
+                    }
+
+                } 
+                catch (FormatException ex)
+                {
+                    Console.WriteLine($"Given input is not an integer,\n please enter input from above,\n exited with: {ex}");
+                    continue;
+
+                } catch (Exception ex)
+                {
+                    Console.WriteLine($"Exited with exeption: {ex}");
+                    continue;
+
+                }
             }
         }
     }
