5190a3592569edc9f0342f74fe311c29b8dcd7dc
Darco <darcowb388@gmail.com>
2025-09-17 15:51:29 +0530

    Second commit

---
+++ FIleManagerConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Main
+{
+    public class  Program
+    {
+        public static void Main()
+        {
+            
+
+            while (true) {
+
+                Console.WriteLine("Hell world");
+
+            }
+        }
+    }
+}
