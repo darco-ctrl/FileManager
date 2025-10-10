c10ff266bcdc31ecb05d982a443ceb3fbf98ba62
Darco <darcowb388@gmail.com>
2025-10-10 22:06:22 +0530

    Megging into UI

---
+++ AvaloniaTest/AvaloniaTest/Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+
+namespace AvaloniaTest.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
